d40ccca5d9de51713df8ad5b55f0d3d5d11785ed
DarkMatterMatt <mattmoran2000@gmail.com>
2019-02-25 13:50:35 +1300

    PROS setup, arcade drive control

---
+++ include/definitions.h
@@ -0,0 +1,18 @@
+#ifndef _DEFINITIONS_H_
+#define _DEFINITIONS_H_
+
+// Motors
+#define DRIVE_LEFT_FRONT    1
+#define DRIVE_LEFT_BACK     2
+#define DRIVE_RIGHT_FRONT   3
+#define DRIVE_RIGHT_BACK    4
+
+// Sensors
+
+
+// Misc
+#define WHEEL_DIAMETER      4_in
+#define CHASSIS_WIDTH       10_in
+#define CONTROLLER_THRESHOLD 0.03
+
+#endif
---
+++ src/opcontrol.cpp
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+void opcontrol() {
+	auto drive = ChassisControllerFactory::create(
+		{ DRIVE_LEFT_FRONT,  DRIVE_LEFT_BACK  },
+		{ DRIVE_RIGHT_FRONT, DRIVE_RIGHT_BACK },
+		AbstractMotor::gearset::green,
+		{ WHEEL_DIAMETER, CHASSIS_WIDTH }
+	);
+
+	Controller ctl;
+	while (true) {
+		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
+		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+		
+		drive.arcade(ctl.getAnalog(okapi::ControllerAnalog::leftY), ctl.getAnalog(okapi::ControllerAnalog::leftX), CONTROLLER_THRESHOLD);
+
+		pros::delay(20);
+	}
+}
